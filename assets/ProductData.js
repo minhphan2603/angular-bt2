c38741298249f6f8b6fa23a7ef4bb154df6f3a55
Minh Phan <minhphan2603@gmail.com>
2019-06-21 17:15:32 +0700

    Auto-generated commit

---
+++ assets/ProductData.js
@@ -0,0 +1,13 @@
+export const ProductData = [
+    { MaSP: 1, TenSP: "Sony XZ", Gia: 1000 },
+    { MaSP: 2, TenSP: "Sony XZ2", Gia: 1000 },
+    { MaSP: 3, TenSP: "HTC U Ultra", Gia: 1000 },
+    { MaSP: 4, TenSP: "HTC U12 Plus", Gia: 1000 },
+    { MaSP: 5, TenSP: "Iphone XS MAX", Gia: 1000 },
+    { MaSP: 6, TenSP: "Iphone XR", Gia: 1000 },
+    { MaSP: 7, TenSP: "Xiaomi Mi Note 3", Gia: 9900 },
+    { MaSP: 8, TenSP: "Xiaomi Mi 8", Gia: 1000 },
+    { MaSP: 9, TenSP: "Galaxy Note 9", Gia: 1000 },
+    { MaSP: 10, TenSP: "Galaxy S9 Plus", Gia: 1000 },
+    { MaSP: 11, TenSP: "Nokia X9", Gia: 1000 },
+];
